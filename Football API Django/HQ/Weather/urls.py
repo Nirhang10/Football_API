5d0189a3e2a16ed603db4427af229a198949c1eb
Nirhang10 <156817911+Nirhang10@users.noreply.github.com>
2025-08-16 16:20:06 +0545

    Delete Football API Django/HQ directory

--- Football API Django/HQ/Weather/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('football/<int:club_id>', views.club_squad, name='football_detail'),
-    path('livematch/', views.match_info, name='match'),
-    path('plmatch/', views.Pl_match, name='PLmatch'),
-]
